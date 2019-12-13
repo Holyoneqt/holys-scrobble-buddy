69257593b33b906eef73cdee79e1f856f9bf75cf
Luca Spühler <luca.spuehler@axa-winterthur.ch>
2019-12-13 16:29:58 +0100

    changes for deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  spotifyClientId: '10992dc9a11345068eedc2783ec3b995',
+  spotifyCallbackUrl: 'http://localhost:4200/login',
+
+  lastfmClientId: '47f7f5fe38d25a1c26b72dd54becd0c5',
 };
