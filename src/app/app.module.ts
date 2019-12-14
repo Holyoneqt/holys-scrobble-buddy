e6ed43e90e7c72fd0a9227a66b1ea7983efd6e03
Luca Spühler <spuehler.luca@gmail.com>
2019-12-14 01:44:53 +0100

    removed flex layout reference

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -38,7 +37,6 @@ import { metaReducers, reducers } from './store/index.store';
         FormsModule,
         ReactiveFormsModule,
         HttpClientModule,
-        FlexLayoutModule,
 
         AppMaterialModule,
         AppRoutingModule,
